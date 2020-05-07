b12367e4d5d5c91de0f9f16c6348e357b2586f16
learnedvector <learnedvector@gmail.com>
2020-05-06 22:28:49 -0500

    save model params

--- VoiceAssistant/senses/wakeword/train.py
+++ VoiceAssistant/senses/wakeword/train.py
@@ -12,9 +12,10 @@ from model import LSTMWakeWord
 from sklearn.metrics import classification_report
 
 
-def save_checkpoint(checkpoint_path, model, optimizer, scheduler, notes=None):
+def save_checkpoint(checkpoint_path, model, optimizer, scheduler, model_params, notes=None):
     torch.save({
-        "notes": str(notes),
+        "notes": notes,
+        "model_params": model_params,
         "model_state_dict": model.state_dict(),
         "optimizer_state_dict": optimizer.state_dict(),
         "scheduler_state_dict": scheduler.state_dict()
@@ -77,8 +78,6 @@ def train(train_loader, model, optimizer, loss_fn, device, epoch):
     return acc
 
 
-
-
 def main(args):
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     torch.manual_seed(1)
@@ -99,8 +98,12 @@ def main(args):
                                         collate_fn=collate_fn,
                                         **kwargs)
 
-    model = LSTMWakeWord(num_classes=1, feature_size=40, hidden_size=128,
-                        num_layers=1, dropout=0.1, bidirectional=False, device=device)
+    model_params = {
+        "num_classes": 1, "feature_size": 40, "hidden_size": 128,
+        "num_layers": 1, "dropout" :0.1, "bidirectional": False,
+        "device": device
+    }
+    model = LSTMWakeWord(**model_params)
     model = model.to(device)
     optimizer = optim.AdamW(model.parameters(), lr=args.lr)
     loss_fn = nn.BCEWithLogitsLoss()
@@ -114,17 +117,19 @@ def main(args):
         train_acc = train(train_loader, model, optimizer, loss_fn, device, epoch)
         test_acc = test(test_loader, model, device, epoch)
 
+        # record best train and test
         if train_acc > best_train_acc:
             best_train_acc = train_acc
         if test_acc > best_test_acc:
             best_test_acc = test_acc
 
+        # saves checkpoint if metrics are good
         if args.save_checkpoint_path and train_acc >= best_train_acc and test_acc >= best_test_acc:
             checkpoint_path = os.path.join(args.save_checkpoint_path, args.model_name + ".pt")
             print("found best checkpoint. saving model as", checkpoint_path)
             save_checkpoint(
-                checkpoint_path, model, optimizer, scheduler,
-                notes="train_acc: {}, test_acc: {}, epoch: {}".format(best_train_acc, best_test_acc, epoch)
+                checkpoint_path, model, optimizer, scheduler, model_params,
+                notes="train_acc: {}, test_acc: {}, epoch: {}".format(best_train_acc, best_test_acc, epoch),
             )
 
         print("\ntrain acc:", train_acc, "test acc:", test_acc, "\n",
@@ -142,18 +147,13 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, default=32, help='size of batch')
     parser.add_argument('--eval_batch_size', type=int, default=32, help='size of batch')
     parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
-    parser.add_argument('--model_name', type=str, default="wakeword", required=False,
-                        help='name of model to save')
-    parser.add_argument('--save_checkpoint_path', type=str, default=None,
-                        help='Path to save the best checkpoint')
-    parser.add_argument('--train_data_json', type=str, default=None, required=True,
-                        help='path to train data json file')
-    parser.add_argument('--test_data_json', type=str, default=None, required=True,
-                        help='path to test data json file')
-    parser.add_argument('--no_cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--num_workers', type=int, default=1,
-                        help='number of data loading workers')
+    parser.add_argument('--model_name', type=str, default="wakeword", required=False, help='name of model to save')
+    parser.add_argument('--save_checkpoint_path', type=str, default=None, help='Path to save the best checkpoint')
+    parser.add_argument('--train_data_json', type=str, default=None, required=True, help='path to train data json file')
+    parser.add_argument('--test_data_json', type=str, default=None, required=True, help='path to test data json file')
+    parser.add_argument('--no_cuda', action='store_true', default=False, help='disables CUDA training')
+    parser.add_argument('--num_workers', type=int, default=1, help='number of data loading workers')
+    parser.add_argument('--hidden_size', type=int, default=128, help='lstm hidden size')
 
     args = parser.parse_args()
 
