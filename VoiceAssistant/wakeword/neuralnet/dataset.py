0c8e09001d386ffbf9d03c47a416057dd04154ac
michael <learnedvector@gmail.com>
2020-05-13 17:34:09 +0100

    rpi

--- VoiceAssistant/wakeword/neuralnet/dataset.py
+++ VoiceAssistant/wakeword/neuralnet/dataset.py
@@ -3,10 +3,29 @@ import torch
 import torch.nn as nn
 import torchaudio
 import pandas as pd
+from sonopy import power_spec, mel_spec, mfcc_spec, filterbanks
+
+
+class MFCC(nn.Module):
+
+    def __init__(self, sample_rate, fft_size=400, window_stride=(400, 200), num_filt=40, num_coeffs=40):
+        super(MFCC, self).__init__()
+        self.sample_rate = sample_rate
+        self.window_stride = window_stride
+        self.fft_size = fft_size
+        self.num_filt = num_filt
+        self.num_coeffs = num_coeffs
+        self.mfcc = lambda x: mfcc_spec(
+            x, self.sample_rate, self.window_stride,
+            self.fft_size, self.num_filt, self.num_coeffs
+        )
+    
+    def forward(self, x):
+        return torch.Tensor(self.mfcc(x.squeeze(0).numpy())).transpose(0, 1).unsqueeze(0)
 
 
 def get_featurizer(sample_rate):
-    return torchaudio.transforms.MFCC(sample_rate=sample_rate, log_mels=True)
+    return MFCC(sample_rate=sample_rate)
 
 
 class RandomCut(nn.Module):
