12fb57d0c76d97ebf97a9e787f6726537ca332a5
Sandeep <sunthapa1@hotmail.com>
2021-03-03 13:01:11 +0000

    added datasets such that the scripts work on fly, on colab

--- VoiceAssistant/nlu/neuralnet/config.py
+++ VoiceAssistant/nlu/neuralnet/config.py
@@ -1,8 +1,8 @@
 import transformers
 import torch
-RAW_DATASET_PATH = '../data/nlu.csv'
-ER_DATASET_PATH  = '../data/er_dataset.csv'
-IS_DATASET_PATH  = '../data/is_dataset.csv'
+RAW_DATASET_PATH = '../nlu_dataset/nlu.csv'
+ER_DATASET_PATH  = '../nlu_dataset/er_dataset.csv'
+IS_DATASET_PATH  = '../nlu_dataset/is_dataset.csv'
 MODEL_PATH = '/Volumes/My Passport 1/models/epoch50_best_model.pth' 
 TRACE_MODEL_PATH ='/Volumes/My Passport 1/models/epoch50_best_model_trace.pth' 
 LOG_PATH = '/home2/ncwn67/A-Hackers-AI-Voice-Assistant/VoiceAssistant/nlu/tb_logs/run4'
